6938b039f0ff9e836d9edc4c4777b04876937b88
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-08 13:07:51 +0600

    using css module

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,16 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+// import "./index";
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
       {/* page will refresh if use a tag for navigation */}
       {/* <a href="/pricing">Pricing</a> */}
-      {/* <Link to="/pricing">Pricing</Link> */}
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
