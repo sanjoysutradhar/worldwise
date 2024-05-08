8c154454d9a9bad838cc9831915b428d34e18ce8
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-08 14:16:59 +0600

    05 binding app layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
-
+// import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
